0b4a94e8dfac1a16f6e671f3440a4d660eff6ded
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2015-08-31 15:19:20 +0900

    refactoring

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,29 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the right title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
   describe "help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the right title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
   end
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    it "should have the right title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
   end
 end
