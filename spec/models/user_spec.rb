2ff10760b5b1769db7a06fc6dbf64ee7a074535b
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2015-09-08 18:52:47 +0900

    8.2.1完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,8 +13,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
-
   it { should be_valid }
   describe "when name is not present" do
     before { @user.name = " " }
@@ -70,26 +70,30 @@ describe User do
     it { should be_invalid }
   end
   describe "return value of authenticate method" do
-      before { @user.save }
-      let(:found_user) { User.find_by(email: @user.email) }
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
 
-      describe "with valid password" do
-        it { should eq found_user.authenticate(@user.password) }
-      end
-
-      describe "with invalid password" do
-        let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-        it { should_not eq user_for_invalid_password }
-        specify { expect(user_for_invalid_password).to be_false }
-      end
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
     end
-    describe "email address with mixed case" do
-      let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
-      it "should be saved as all lower-case" do
-        @user.email = mixed_case_email
-        @user.save
-        expect(@user.reload.email).to eq mixed_case_email.downcase
-      end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
