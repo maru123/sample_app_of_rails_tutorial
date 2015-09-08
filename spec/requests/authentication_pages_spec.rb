df4481d882a429f611ccb8d78c3bdf4814513ac1
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2015-09-08 20:20:16 +0900

    8.2.4完了

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,13 +20,12 @@ describe "Authentication" do
       end
     end
     describe "with valid informtion" do
-      let(:user) { FactoryGorl.create(:user) }
+      let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email",    with: user.email.upcase
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
-
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
